52028f0c66e25919e9c1b7e9a7d1e9e2c5829b5d
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-04-10 16:02:59 +1000

    complete using state

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("Shaun");
+  const [person, setPerson] = useState({ name: "Mario", age: 40 });
+
+  const clickHandler = () => {
+    setName("Arjun");
+    setPerson({
+      name: "Matthew",
+      age: 45,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}.</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}.
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update state" onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -13,8 +31,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
